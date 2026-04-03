11b6050633fe4d43cd75750c158bd44a9f1b1413
shreyaswaghade1245-pixel <shreyaswaghade1245@gmail.com>
2026-04-03 19:44:32 +0530

    Delete Snaply-Full-Stack-Social-Media-Platform-main/Snaply-Full-Stack-Social-Media-Platform-main/backend directory

--- Snaply-Full-Stack-Social-Media-Platform-main/Snaply-Full-Stack-Social-Media-Platform-main/backend/routes/message.routes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const isAuthenticated = require('../middlewares/isauthenticated');
-const { sendMessage, getMessages } = require('../controllers/message.controller');
-const route = express.Router();
-
-route.post('/send/:id', isAuthenticated, sendMessage);
-route.get('/all/:id', isAuthenticated, getMessages);
-
-module.exports = route;
